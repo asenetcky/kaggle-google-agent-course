baf7878767e7ed8d9508f62bb782237d027ceec2
asenetcky <me@asenetcky.dev>
2025-11-18 07:22:08 -0500

    feat: add setup to part2

--- day04-part02.py
+++ day04-part02.py
@@ -59,6 +59,75 @@ def _(mo):
 
 
 @app.cell
+def _(mo):
+    mo.md(r"""
+    Previously, our practice of implementation of
+    implementing observability in AI agents was
+    primarily **reactive** - it comes into play only
+    after an issue ocurred.
+
+    This notebook will complement those practices with
+    a *proactive* approach using **Agent Evaluation**.
+    We will be continuously evaluating our agent's
+    performance and catch quality degradation much
+    earlier.
+    """)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    ### The story
+    """)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    Pretend for a moment - we've built a home automation
+    agent. Works and tests perfectly so we launch it
+    in full confidence.
+
+    Then the following occurs:
+
+    - **Week 1:** Agent turns on fireplace when asked for
+        lights
+    - **Week 2:** Agent wont respond to commands in guest
+      room.
+    - **Week 3:** Agent gives rude responses when devices
+      are unavailable
+    """)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    > **The Problem:** `Standard testing does not equal
+    > Evaluation
+
+    Agents are different from traditional software:
+
+    - They are *non-deterministic*
+    - Users give unpredictable, ambiguous commands
+    - Small prompt changes cause dramatic behavior
+      shifts and different tool calls
+
+      To accommodate all these variables, agents need
+      systematic evaluation, not just the `happy path`
+      testing.
+
+      > The agent's entire decision-making process -
+      > including the final response, and the path
+      > it took to get the response (trajectory)
+      > needs to be assessed.
+    """)
+    return
+
+
+@app.cell
 def _():
     return
 
